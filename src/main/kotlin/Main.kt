d5086377b733d52174e1448ba6500d852e3a7683
Joan Njiru <joanwanjirunjiru@gmail.com>
2023-02-02 09:01:22 +0300

    variables and data types

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    var fullName = "Jane Doe"
+    var age = 12
+    var telephoneNumber = "074566666"
+    var weightInKgs = 60
+    val isKenyan: Boolean = true
+    val notKenyan: Boolean = false
+
+
+
+
+    println(isKenyan)
+
+
+}
